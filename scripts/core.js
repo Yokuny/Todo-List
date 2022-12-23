6e3e85325612bf7e7779ff2805d39e9b9ebf0636
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-23 13:40:10 -0300

    testing

---
+++ scripts/core.js
@@ -0,0 +1,16 @@
+/* As tarefas são Factory */
+/* obrigatoriamente devem ter title, description, dataDeVencimento and priority  */
+/* pode adicionar Notas e Checklist */
+/* deve haver lista de diferentes tipos de projetos para agrupar as tarefas */
+/* quando o usuario abrir o aplicativo deve haver todo padrões e exemplo de conjunto */
+/* o Usuario deve poder criar Novos conjuntos e um opção que selecione aonde seus todo irão ser adicionados */
+/* Você deve separar a lógica do seu aplicativo (ou seja, criar novos todos, definir todos como completos,
+    alterar a prioridade do todo, etc.) do material relacionado ao DOM, portanto, mantenha todas essas coisas em módulos separados. */
+/* a interface deve ter
+- visualizar os projetos (pastas de to do)
+- visualizar todos as tarefas em cada projeto (provavelmente apenas o título e a data de vencimento…
+    mudando de cor para diferentes prioridades)
+- expandir uma unica tarefa para poder ver seus detalhes e altera-la
+- deletar uma tarefa
+*/
+/* implementar forma de salvamento das tarefas em localStorage > salvando os arquivos no computador do usuario */
